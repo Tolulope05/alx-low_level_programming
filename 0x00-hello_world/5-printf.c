567ad23a8a93baeccfc841d0d1016a5f60078f9c
Tolulope05 <fakunletolulope05@gmail.com>
2022-03-10 09:12:07 -0800

    Hello print

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,7 +3,7 @@
 /**
  * main - Entry point
  *
- * Description: prints "with proper grammar, but the outcome is a piece of art."
+ * Description: prints "with proper grammar, but the outcome is a piece of art,"
  *
  * Return: Always 0 (Success)
  */
