99576d21c837fc90466f43bba1fe219de4b0133f
Tolulope05 <fakunletolulope05@gmail.com>
2022-03-10 09:31:30 -0800

    Size is not grandeur

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,7 +3,8 @@
 /**
  * main - print the size of various types
  *
- * Description: prints the size of various types on the computer it is compiled and run on
+ * Description: prints the size of various types on the computer it is compiled
+ * and run on
  *
  * Return: Allways 0 (Success)
  */
