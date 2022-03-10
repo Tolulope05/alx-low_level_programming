402281bccbbaa9d7a2e53ac90f4901f0366fd2c0
Tolulope05 <fakunletolulope05@gmail.com>
2022-03-10 09:28:38 -0800

    Size is not grandular, and territory does not make a nation

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - print the size of various types
+ *
+ * Description: prints the size of various types on the computer it is compiled and run on
+ *
+ * Return: Allways 0 (Success)
+ */
+
+int main(void)
+{
+	printf("Size of a char: %d byte(s)\n", (int) sizeof(char));
+	printf("Size of an int: %d byte(s)\n", (int) sizeof(int));
+	printf("Size of a long int: %d byte(s)\n", (int) sizeof(long int));
+	printf("Size of a long long int: %d byte(s)\n", (int) sizeof(long long int));
+	printf("Size of a float: %d byte(s)\n", (int) sizeof(float));
+
+	return (0);
+}
