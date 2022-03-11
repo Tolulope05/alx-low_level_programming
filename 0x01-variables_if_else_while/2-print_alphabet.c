977197243a214f4c622b0d82034b941ec0d3b523
Tolulope05 <fakunletolulope05@gmail.com>
2022-03-11 10:50:01 -0800

    I sometimes suffer from insomnia. and when i cant fall asleep, icant type

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - print the letters of the alphabet
+ *
+ * Description: print the letters of the alphabet
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i = 97;
+
+	while (i < 123)
+	{
+		putchar(i);
+		i++;
+	}
+	putchar(10);
+
+	return (0);
+}
