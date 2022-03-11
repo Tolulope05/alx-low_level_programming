c796acdafd94828e2660b87382cc56d538964202
Tolulope05 <fakunletolulope05@gmail.com>
2022-03-11 11:04:09 -0800

    Task 4, cant talk too much

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - print the letters of the alphabet
+ *
+ * Description: print the letters of the alphabet except e, q
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{ int i = 97;
+
+	while (i < 123)
+	{
+		if (i != 101 && i != 113)
+		{
+			putchar(i);
+		}
+		i++;
+	}
+	putchar(10);
+
+	return (0);
+}
