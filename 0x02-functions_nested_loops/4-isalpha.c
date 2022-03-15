908102a709af6f6e0a3ce1bb3be7bfa036013171
Tolulope05 <fakunletolulope05@gmail.com>
2022-03-15 14:14:56 -0700

    alpa

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -4,6 +4,7 @@ include "main.h"
  * @c: type int character
  * Return: 1 if letter, lowercase or uppercase, and 0 otherwise
  */
+
 int _isalpha(int c)
 {
 	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
