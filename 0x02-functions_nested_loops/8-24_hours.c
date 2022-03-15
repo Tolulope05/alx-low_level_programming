3169272e1be0526dba3bd0f437502393d59bd400
Tolulope05 <fakunletolulope05@gmail.com>
2022-03-15 14:41:20 -0700

    heyy

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * jack_bauer - prints time
+ *
+ * Description: prints a list of time
+ * Return: Always 0.
+ */
+
+void jack_bauer(void)
+{
+	int i, j;
+
+	for (i = 0; i < 24; i++)
+	{
+		for (j = 0; j < 60; j++)
+		{
+			_putchar(i / 10 + 48);
+			_putchar(i % 10 + 48);
+			_putchar(':');
+			_putchar(j / 10 + 48);
+			_putchar(j % 10 + 48);
+			_putchar('\n');
+		}
+	}
+}
+
