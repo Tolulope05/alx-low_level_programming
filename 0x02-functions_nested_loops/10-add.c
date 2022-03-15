8522bd5c6836a71fcd8ac18c90f55cd0264858e7
Tolulope05 <fakunletolulope05@gmail.com>
2022-03-15 15:07:27 -0700

    asdd

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/*
+ * add - add two numbers from input
+ * @a: first aparamet
+ * @b: second parameter
+ *
+ * Description: adds two numbers
+ * Return: Always (0).
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
