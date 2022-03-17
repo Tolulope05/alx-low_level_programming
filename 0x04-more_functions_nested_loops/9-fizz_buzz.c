fcf70a1b078bfdbf3fa6c97b169e70296324cf65
Tolulope05 <fakunletolulope05@gmail.com>
2022-03-17 11:58:34 -0700

    heyyyy

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -19,6 +19,9 @@ int main(void)
 		} else if (i % 5 == 0 && i % 3 != 0)
 		{
 			printf(" Buzz");
+		} else if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf(" FizzBuzz");
 		} else if (i == 1)
 		{
 			printf("%d", i);
