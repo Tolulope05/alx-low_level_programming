fe553c3f684dc8fe260c31645386cdce33ed6d66
Tolulope05 <fakunletolulope05@gmail.com>
2022-03-17 06:49:42 -0700

    main

---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+<h1> Nested loops</h1>
---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,17 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+int _isupper(int c);
+int _isdigit(int c);
+int mul(int a, int b);
+void print_numbers(void);
+int _putchar(char c);
+void print_most_numbers(void);
+void more_numbers(void);
+void print_line(int n);
+void print_diagonal(int n);
+void print_square(int size);
+void print_triangle(int size);
+void print_number(int n);
+
+#endif
