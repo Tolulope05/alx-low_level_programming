ed881358b6e4048eb5402dae07a101d408ca31d0
Tolulope05 <fakunletolulope05@gmail.com>
2022-03-21 08:42:41 -0700

    C - reset

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * reset_to_98 - function that takes a pointer to an integer parameter
+ * @n: integer pointer
+ * Return: 0 Success
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+<h1>C - Pointers</h1>
