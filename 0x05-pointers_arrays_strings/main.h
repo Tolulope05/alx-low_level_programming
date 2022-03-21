262bae96bdd463c7a83af88f2d8e824d6a640f1c
Tolulope05 <fakunletolulope05@gmail.com>
2022-03-21 09:20:33 -0700

    Correct main

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -6,7 +6,7 @@ void print_rev(char *s);
 void rev_string(char *s);
 void puts2(char *str);
 void puts_half(char *str);
-void _puthchar(char);
+void _putchar(char);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
