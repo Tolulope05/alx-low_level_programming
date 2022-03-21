6f9c54e4f7e6cdd389f60ead6cee05a715562201
Tolulope05 <fakunletolulope05@gmail.com>
2022-03-21 09:03:10 -0700

    File executable

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,9 +1,11 @@
 #include "main.h"
+
 /**
  * reset_to_98 - function that takes a pointer to an integer parameter
  * @n: integer pointer
  * Return: 0 Success
  */
+
 void reset_to_98(int *n)
 {
 	*n = 98;
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,6 +1,3 @@
-#ifndef MAIN_H
-#define MAIN_H
-
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
@@ -9,10 +6,7 @@ void print_rev(char *s);
 void rev_string(char *s);
 void puts2(char *str);
 void puts_half(char *str);
+void _puthchar(char);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
-int _keygen.c(char *s)
-void _putchar(char c);
-
-#endif
