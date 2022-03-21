c08ae9459869f436cc373ec64dacb69eb4ef9c10
Tolulope05 <fakunletolulope05@gmail.com>
2022-03-21 08:49:12 -0700

    heyy

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -12,7 +12,7 @@ void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
-int _keygen.c(char *s);
+int _keygen.c(char *s)
 void _putchar(char c);
 
 #endif
