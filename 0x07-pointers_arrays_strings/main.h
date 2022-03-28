90a7df61cf6df15d0a1f3a8137db4a2a003c4e74
Fakunle Tolulope <fakunletolulope05@gmail.com>
2022-03-28 16:44:52 +0100

    Update main.h

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,1 +1,15 @@
-Hey
+#ifndef _HOLBERTON_H_
+#define _HOLBERTON_H_
+
+char *_memset(char *s, char b, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n);
+char *_strchr(char *s, char c);
+unsigned int _strspn(char *s, char *accept);
+char *_strpbrk(char *s, char *accept);
+char *_strstr(char *haystack, char *needle);
+int _putchar(char);
+void print_chessboard(char (*a)[8]);
+void print_diagsums(int *a, int size);
+void set_string(char **s, char *to);
+
+#endif
