21a0db092e009cde13ad288a75a95faf8c0724f8
Fakunle Tolulope <fakunletolulope05@gmail.com>
2022-03-30 13:36:12 +0100

    Update main.h

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,1 +1,15 @@
-hi
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+int _putchar(char);
+void _puts_recursion(char *s);
+void _print_rev_recursion(char *s);
+int _strlen_recursion(char *s);
+int factorial(int n);
+int _pow_recursion(int x, int y);
+int _sqrt_recursion(int n);
+int is_prime_number(int n);
+int is_palindrome(char *s);
+int wildcmp(char *s1, char *s2);
+
+#endif
