e9ced0e15a5b6edc8e7661132b8e706536f369f0
Fakunle Tolulope <fakunletolulope05@gmail.com>
2022-04-01 15:41:20 +0100

    Update 2-strlen.c

---
+++ 0x09-static_libraries/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the length of a string.
+ * @s: input stringeturn.
+ * Return: length of a string.
+ */
+int _strlen(char *s)
+{
+	int count = 0;
+
+	while (*(s + count) != '\0')
+		count++;
+	return (count);
+}
