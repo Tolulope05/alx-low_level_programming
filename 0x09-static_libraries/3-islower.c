580a92a54780e2587817a386b5f8a2a2d58f354d
Fakunle Tolulope <fakunletolulope05@gmail.com>
2022-04-01 15:42:51 +0100

    Update 3-islower.c

---
+++ 0x09-static_libraries/3-islower.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _islower - prints 1 or 0 depending on input
+ * @c: first parameter
+ *
+ * Description: prints all lowercase letters
+ * Return: Always(0).
+ */
+
+int _islower(int c)
+{
+	return (c >= 'a' && c <= 'z');
+}
