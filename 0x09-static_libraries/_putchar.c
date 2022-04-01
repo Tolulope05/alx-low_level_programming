6edb0e0ec8f8c362fe3e3d21fb5fb24cad3d437c
Fakunle Tolulope <fakunletolulope05@gmail.com>
2022-04-01 16:19:58 +0100

    Update _putchar.c

--- 0x09-static_libraries/_putchar.c
+++ 0x09-static_libraries/_putchar.c
@@ -1,24 +1,13 @@
-#include "main.h"
+#include <unistd.h>
 
 /**
- * main - prints Putchar
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
  *
- * Description: prints _putchar
- *
- * Return: 0 (Success)
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
  */
-
-int main(void)
+int _putchar(char c)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
-
-	return (0);
+	return (write(1, &c, 1));
 }
