2599c366e82db73f21e82c5f5aace80d201b95fc
Fakunle Tolulope <fakunletolulope05@gmail.com>
2022-04-01 15:51:50 +0100

    Update _putchar.c

---
+++ 0x09-static_libraries/_putchar.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * main - prints Putchar
+ *
+ * Description: prints _putchar
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
+}
