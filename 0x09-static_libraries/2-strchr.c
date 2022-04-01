742e8e31713c2f3bb622d6118e8b45a5e1739421
Fakunle Tolulope <fakunletolulope05@gmail.com>
2022-04-01 15:40:41 +0100

    Update 2-strchr.c

---
+++ 0x09-static_libraries/2-strchr.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _strchr - locates a character in a string,
+ * @s: string.
+ * @c: character.
+ * Return: the pointer to the first occurrence of the character c.
+ */
+char *_strchr(char *s, char c)
+{
+	unsigned int i = 0;
+
+	for (; *(s + i) != '\0'; i++)
+		if (*(s + i) == c)
+			return (s + i);
+	if (*(s + i) == c)
+		return (s + i);
+	return ('\0');
+}
