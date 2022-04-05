80df20c2e5a8daf435c57cfe3b2a316344096e8e
Fakunle Tolulope <fakunletolulope05@gmail.com>
2022-04-05 06:46:56 +0100

    Update main.h

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -1,1 +1,11 @@
-hi
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+char *create_array(unsigned int size, char c);
+char *_strdup(char *str);
+char *str_concat(char *s1, char *s2);
+int **alloc_grid(int width, int height);
+void free_grid(int **grid, int height);
+char *argstostr(int ac, char **av);
+
+#endif
