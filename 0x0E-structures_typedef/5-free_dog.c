068b2cad8eccabd70fb57329344bc85c3593394d
Fakunle Tolulope <fakunletolulope05@gmail.com>
2022-04-11 15:07:44 +0100

    Update 5-free_dog.c

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -1,1 +1,18 @@
-ls
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * free_dog - frees dogs.
+ * @d: struct dog.
+ *
+ * Return: no return.
+ */
+void free_dog(dog_t *d)
+{
+	if (d)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
+}
