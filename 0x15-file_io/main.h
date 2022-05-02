ae9653fbc4529d1dea7de79186f17d9e96bf6f40
Fakunle Tolulope <fakunletolulope05@gmail.com>
2022-05-02 16:07:53 +0100

    Update main.h

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,1 +1,14 @@
-hi
+#ifndef __MAIN_H__
+#define __MAIN_H__
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
+int append_text_to_file(const char *filename, char *text_content);
+
+#endif
