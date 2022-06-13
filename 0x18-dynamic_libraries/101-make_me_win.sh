9da5ec8218cf2b2f55b0da7920c9419352223811
Fakunle Tolulope <fakunletolulope05@gmail.com>
2022-06-13 17:21:18 +0100

    Make me a wish success

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/Tolulope05/alx-low_level_programming/edit/master/0x18-dynamic_libraries/libmask.so
+wget -P /tmp https://github.com/Tolulope05/alx-low_level_programming/blob/master/0x18-dynamic_libraries/libdynamic.so
 export LD_PRELOAD=/tmp/libmask.so
