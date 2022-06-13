e0ef3bf9a2a7939819311568ea48143f6c0a4764
Fakunle Tolulope <fakunletolulope05@gmail.com>
2022-06-13 17:50:38 +0100

    finally i won

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 wget -P /tmp https://github.com/Tolulope05/alx-low_level_programming/blob/master/0x18-dynamic_libraries/nrandom.so
-export LD_PRELOAD=/tmp/libmask.so
+export LD_PRELOAD=/tmp/nrandom.so
